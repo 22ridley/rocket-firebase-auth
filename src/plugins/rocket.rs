404ddc01bc5c246f4010277cdaa4ee5983b6b43b
Sarah Ridley <sarah_ridley@brown.edu>
2024-04-09 13:25:35 -0400

    compatible with alohomora

--- src/plugins/rocket.rs
+++ src/plugins/rocket.rs
@@ -26,16 +26,16 @@ impl<'r> FromRequest<'r> for FirebaseToken {
             (Some(bearer_token), Some(firebase_auth)) => {
                 match firebase_auth.verify(bearer_token.as_str()).await {
                     Ok(firebase_token) => Outcome::Success(firebase_token),
-                    Err(_) => Outcome::Error((
+                    Err(_) => Outcome::Failure((
                         Status::Unauthorized,
                         Error::InvalidJwt(InvalidJwt::Unspecified),
                     )),
                 }
             }
             (Some(_), None) => {
-                Outcome::Error((Status::Unauthorized, Error::FirebaseAuthStateNotFound))
+                Outcome::Failure((Status::Unauthorized, Error::FirebaseAuthStateNotFound))
             }
-            _ => Outcome::Error((
+            _ => Outcome::Failure((
                 Status::BadRequest,
                 Error::InvalidAuthHeader(InvalidAuthHeader::InvalidFormat(
                     "Failed to parse bearer token.".to_string(),
